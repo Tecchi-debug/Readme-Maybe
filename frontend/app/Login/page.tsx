935497b31f9c979467e088f219024a583635c5a8
Tecchi-debug <saa2468159@gmail.com>
2026-03-23 23:18:27 -0400

    Login/Signup

--- frontend/app/Login/page.tsx
+++ frontend/app/Login/page.tsx
@@ -1,11 +1,5 @@
-import PageTitle from "../components/PageTitle";
 import Login from "../components/Login";
 const LoginPage = () => {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <PageTitle />
-      <Login />
-    </div>
-  );
+  return <Login />;
 };
 export default LoginPage;
