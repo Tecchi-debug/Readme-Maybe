53e6f7ebc067473ebdfd18f0cedc2eece1cc7fbf
Aiden Sperr <asperr@8ksolutions.com>
2026-03-25 10:37:25 -0400

    Setup, changed CardPage to Dashboard, started dashboard setup

---
+++ frontend/app/components/Dashboard.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+function Dashboard() {
+  return (
+        <div className="flex w-full h-screen">
+            <div className="bg-[#1c1a2e] w-[220px]">
+              
+            </div>
+            <div className="bg-[#13111e] flex-1">
+              
+            </div>
+        </div>
+    );
+}
+export default Dashboard;
